6c250cad4aa733bbc1283dd098018cbf091b8cf4
Cairo <cairosanders@hotmail.com>
2020-06-20 15:08:09 -0700

    one button for voting

--- src/ResultVote.js
+++ src/ResultVote.js
@@ -1,14 +1,22 @@
 
 import React from 'react';
 
-
-
 class ResultVote extends React.Component {
 
-    constructor(props) {
+  constructor(props) {
         super(props);
-    }
+        this.promptAnswers = ["a","b","c"];
+        this.votes = new Array(this.promptAnswers.length).fill(0);
+  }
 
+  clicked(index){
+    this.votes[index] +=1;
+    console.log(this.votes);
+  }
 
-
-} export default ResultVote
+  render() {
+    return <div>
+      <button onClick={ (e) => {this.clicked(0)}}>button</button>
+    </div>;
+  }
+}export default ResultVote
