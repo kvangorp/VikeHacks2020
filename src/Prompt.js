42785f695e0186f662d12a81f899f0e28246717a
becca <beccabthomson@gmail.com>
2020-06-20 12:29:07 -0700

    structure

--- src/Prompt.js
+++ src/Prompt.js
@@ -1,32 +1,11 @@
 import React from 'react';
-import Square from './Square';
 
 class Prompt extends React.Component {
   // Create the prompt
-  createPrompt(row, col) {
-    const board = [];
-    let cellCounter = 0;
-
-    for (let i = 0; i < row; i += 1) {
-      const columns = [];
-      for (let j = 0; j < col; j += 1) {
-        columns.push(this.renderSquare(cellCounter++));
-      }
-      board.push(<div key={i} className="board-row">{columns}</div>);
-    }
-
-    return board;
+  createPrompt(playerName) {
+    
   }
 
-  renderSquare(i) {
-    return (
-      <Square
-        key={i}
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
-  }
 
   render() {
     return <div>{this.createPrompt(3, 3)}</div>;
