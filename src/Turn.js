94067170dc11573d595f49ee6fa26a11df53f36d
becca <beccabthomson@gmail.com>
2020-06-20 12:35:17 -0700

    nothing

--- src/Turn.js
+++ src/Turn.js
@@ -11,4 +11,14 @@ class Turn extends React.Component {
         super(props);
     }
 
+
+    /* basic idea:
+     * Create Prompt component (time limit)
+     * Get user input from Prompt (pubnub) and create Vote component
+     * Get user input from Vote (pubnub) and show Results
+     * (ideally track score)
+     */
+
+
+
 }
