49369c1749f5140fb60df6dd029317774ed5116e
becca <beccabthomson@gmail.com>
2020-06-20 12:32:17 -0700

    basically nothing

--- src/Turn.js
+++ src/Turn.js
@@ -1,3 +1,14 @@
 import Prompt from './Prompt';
 import Vote from './Prompt';
 import Results from './Prompt';
+import React from 'react';
+
+
+
+class Turn extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+}
