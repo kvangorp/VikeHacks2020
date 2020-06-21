f8360792a51b09366d06796e59dd18f4cc12051d
kvangorp <katelyn.vangorp@gmail.com>
2020-06-21 08:01:00 -0700

    Updates

---
+++ precache-manifest.9ee8f7eabdb9c0bdbd6f5ab519e16561.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de19a7ddc78c42b0b94152ba943d8786",
+    "url": "/vikehacks2020/index.html"
+  },
+  {
+    "revision": "c59caa69b2f1ab6ce333",
+    "url": "/vikehacks2020/static/css/main.592ca0ec.chunk.css"
+  },
+  {
+    "revision": "d98423f08854c15f370e",
+    "url": "/vikehacks2020/static/js/2.5f076505.chunk.js"
+  },
+  {
+    "revision": "32ad5e7eab35d7758a64b4509f4a87ec",
+    "url": "/vikehacks2020/static/js/2.5f076505.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c59caa69b2f1ab6ce333",
+    "url": "/vikehacks2020/static/js/main.e84a71f7.chunk.js"
+  },
+  {
+    "revision": "407e3a5795cdf05d28f5",
+    "url": "/vikehacks2020/static/js/runtime-main.b4b3cf7c.js"
+  }
+]);
